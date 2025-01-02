b99d48b3702eb1464201c5543eda2e834eccad1c
Ong Hin Journ <onghinjourn@gmail.com>
2025-01-03 00:08:59 +0800

    ci: :hammer: adds postbuild script to copy chuni tools scripts to cdn

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -1,12 +1,59 @@
-import path from 'node:path';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import fs from "node:fs";
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import { defineConfig, type PluginOption } from "vite";
+
+const postBuild = (): PluginOption => {
+  return {
+    name: "postbuild",
+    closeBundle: () => {
+      console.info("\x1b[32mrunning postbuild script...\x1b[0m");
+
+      const scriptsPath = path.join(
+        import.meta.dirname,
+        "/node_modules/@chuni-tools/scripts/dist",
+      );
+      const exists = fs.existsSync(scriptsPath);
+
+      if (!exists) {
+        const error = new Error(
+          "@chuni-tools/scripts not found, skipping postbuild script",
+        );
+        console.error(`\x1b[31m${error.message}\x1b[0m`);
+        throw error;
+      }
+
+      const jsFilePaths = path.join(scriptsPath, "/*.js");
+      const scriptFilePaths = fs.globSync(jsFilePaths);
+
+      if (scriptFilePaths.length === 0) {
+        const error = new Error(
+          "@chuni-tools/scripts not found, skipping postbuild script",
+        );
+        console.error(`\x1b[31m${error.message}\x1b[0m`);
+        throw error;
+      }
+
+      for (const scriptFilePath of scriptFilePaths) {
+        const fileName = path.basename(scriptFilePath);
+        const docsBuildPath = path.join(
+          import.meta.dirname,
+          `/dist/${fileName}`,
+        );
+        fs.copyFileSync(scriptFilePath, docsBuildPath);
+        console.info(`\x1b[34mcopying ${fileName}\x1b[0m`);
+      }
+
+      console.info("\x1b[32mpostbuild script completed\x1b[0m");
+    },
+  };
+};
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), postBuild()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
